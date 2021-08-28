137f8f15ea66caf6ccc80d9c9218033e04f13588
Summerchel <85677955+Summerchel@users.noreply.github.com>
2021-08-28 14:15:47 +0300

    Add some changes in Task3 and change name to tests

---
+++ src/HomeWork 6: Stas Pshon/Test_Task3.py
@@ -0,0 +1,45 @@
+"""Оформите указанную задачу из прошлых домашних в виде функции и покройте
+тестами. Учтите, что в функцию могут быть переданы некорректные значения,
+здесь может пригодится ‘assertRaises’.
+Не нужно переделывать функцию для того чтобы она ловила все возможные
+ситуации сама.
+
+ДЗ 5 задача 3
+
+Реализовать функцию get_ranges которая получает на вход непустой список
+неповторяющихся целых чисел, отсортированных по возрастанию, которая этот
+список “сворачивает”
+ get_ranges([0, 1, 2, 3, 4, 7, 8, 10]) // "0-4,7-8,10"
+ get_ranges([4,7,10]) // "4,7,10"
+ get_ranges([2, 3, 8, 9]) // "2-3,8-9"""
+
+
+from ddt import data
+from ddt import ddt
+from ddt import unpack
+from Task3 import get_ranges
+import unittest
+
+
+@ddt
+class TestGetRangesCase(unittest.TestCase):
+    @data(
+        (get_ranges([0, 1, 2, 3, 4, 7, 8, 10]), "0-4, 7-8, 10"),
+        (get_ranges([4, 7, 10]), "4, 7, 10"),
+        (get_ranges([2, 3, 8, 9]), "2-3, 8-9"),
+    )
+    @unpack
+    def test_main(self, input_name, output):
+        self.assertEqual(input_name, output)
+        with self.assertRaises(TypeError):
+            get_ranges('1, 2, 3')
+        with self.assertRaises(TypeError):
+            get_ranges(1)
+        with self.assertRaises(TypeError):
+            get_ranges({1, 2, 3})
+        with self.assertRaises(IndexError):
+            get_ranges([])
+
+
+if __name__ == '__main__':
+    unittest.main()
