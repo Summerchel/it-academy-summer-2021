6c57776268df8833a10c75889f34c7ff84342d00
Summerchel <85677955+Summerchel@users.noreply.github.com>
2021-09-21 04:49:46 +0300

    Add some fixes in Task3 and Test_Task3 files

--- src/HomeWork 6: Stas Pshon/Test_Task3.py
+++ src/HomeWork 6: Stas Pshon/Test_Task3.py
@@ -13,7 +13,6 @@
  get_ranges([4,7,10]) // "4,7,10"
  get_ranges([2, 3, 8, 9]) // "2-3,8-9"""
 
-
 from ddt import data
 from ddt import ddt
 from ddt import unpack
@@ -22,24 +21,35 @@ import unittest
 
 
 @ddt
-class TestGetRangesCase(unittest.TestCase):
-    @data(
-        (get_ranges([0, 1, 2, 3, 4, 7, 8, 10]), "0-4, 7-8, 10"),
-        (get_ranges([4, 7, 10]), "4, 7, 10"),
-        (get_ranges([2, 3, 8, 9]), "2-3, 8-9"),
-    )
-    @unpack
-    def test_main(self, input_name, output):
-        self.assertEqual(input_name, output)
+class TestGetRanges(unittest.TestCase):
+
+    def test_area(self):
+        self.assertEqual(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]), "0-4, 7-8, 10")
+        self.assertEqual(get_ranges([4, 7, 10]), "4, 7, 10")
+        self.assertEqual(get_ranges([2, 3, 8, 9]), "2-3, 8-9")
+
+    def test_type(self):
         with self.assertRaises(TypeError):
             get_ranges('1, 2, 3')
         with self.assertRaises(TypeError):
             get_ranges(1)
         with self.assertRaises(TypeError):
             get_ranges({1, 2, 3})
+
+    def test_index(self):
         with self.assertRaises(IndexError):
             get_ranges([])
 
+    def test_value(self):
+        with self.assertRaises(ValueError):
+            get_ranges([0, 3, 2, 4, 7, 8, 10])
+            with self.assertRaises(ValueError):
+                get_ranges([0, 2, 2])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_name(self):
+        with self.assertRaises(NameError):
+            get_ranges([k])
+        with self.assertRaises(NameError):
+            get_ranges([a, b, c])
+
+
