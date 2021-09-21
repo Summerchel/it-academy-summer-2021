6750224f085993c5d5d25bb2435fc1877f05d863
Summerchel <85677955+Summerchel@users.noreply.github.com>
2021-09-21 11:52:28 +0300

    Add files via upload

--- src/HomeWork 6: Stas Pshon/main.py
+++ src/HomeWork 6: Stas Pshon/main.py
@@ -13,31 +13,31 @@
  get_ranges([4,7,10]) // "4,7,10"
  get_ranges([2, 3, 8, 9]) // "2-3,8-9"""
 
-from main import get_ranges
-import unittest
 
+def get_ranges(lst):
+    segment = 0
+    lst_temp = [[]]
+    final_str = ''
 
-class TestGetRanges(unittest.TestCase):
+    for num in range(len(lst)):
+        if num != len(lst) - 1:
+            if lst[num] + 1 == lst[num + 1]:
+                (lst_temp[segment].append(lst[num]))
+            elif lst[num] + 1 > lst[num + 1]:
+                raise ValueError("The list should increase and not decrease")
+            else:
+                lst_temp.append([])
+                lst_temp[segment].append(lst[num])
+                segment += 1
+        else:
+            lst_temp[segment].append(lst[num])
 
-    def test_area(self):
-        self.assertEqual(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]), "0-4, 7-8, 10")
-        self.assertEqual(get_ranges([4, 7, 10]), "4, 7, 10")
-        self.assertEqual(get_ranges([2, 3, 8, 9]), "2-3, 8-9")
+    for el in lst_temp:
+        if len(el) > 1:
+            final_str += str(el[0]) + '-' + str(el[-1]) + ', '
+        else:
+            final_str += str(el[0]) + ', '
 
-    def test_index(self):
-        with self.assertRaises(IndexError):
-            get_ranges([])
+    final_str = final_str.rstrip(', ')
 
-    def test_type(self):
-        with self.assertRaises(TypeError):
-            get_ranges('1, 2, 3')
-        with self.assertRaises(TypeError):
-            get_ranges(1)
-        with self.assertRaises(TypeError):
-            get_ranges({1, 2, 3})
-
-    def test_value(self):
-        with self.assertRaises(ValueError):
-            get_ranges([0, 3, 2, 4, 7, 8, 10])
-        with self.assertRaises(ValueError):
-            get_ranges([0, 2, 2])
+    return final_str
