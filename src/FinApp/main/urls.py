42337a369f6d27a77177996bc70cb03f996b5538
Summerchel <85677955+Summerchel@users.noreply.github.com>
2021-10-01 16:43:40 +0300

    Add files

--- src/FinApp/main/urls.py
+++ src/FinApp/main/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('add_spend', views.calc, name='add_spend'),
-    path('about', views.about, name='about')
+    path('add_category', views.add_cat, name='add_cat'),
+    path('about', views.about, name='about'),
+    path('categoty/<int:cat_id>/', views.show_category, name='category'),
+    path('spends_by_date', views.spend_by_date, name='spends_by_date'),
+    path('spends_by_date/day', views.spend_by_day, name='spends_by_day'),
+    path('spends_by_date/month', views.spend_by_month, name='spends_by_month'),
+    path('spends_by_date/year', views.spend_by_year, name='spends_by_year'),
 ]
