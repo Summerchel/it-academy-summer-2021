6615ff1a09144916c79573f58dc6dbee3e27b608
Summerchel <85677955+Summerchel@users.noreply.github.com>
2021-10-02 13:43:51 +0300

    Add files via upload

--- src/FinApp/FinApp/urls.py
+++ src/FinApp/FinApp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import main.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
 ]
+
